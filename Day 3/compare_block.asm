ca54a8d3ffaa0f0cf564ee353fecef8dc76eb213
Akshay Jha <akshayjha@live.in>
2017-08-26 21:38:28 +0530

    Update compare_block.asm

--- Day 3/compare_block.asm
+++ Day 3/compare_block.asm
@@ -1,16 +1,19 @@
+Check if two arrays of memory locations have same content. Return 1 for True and 0 otherwise.
+  
         LXI H, E100H
-        MVI C, 10H
+        MVI C, 0FH
+        MVI D, 01H
 LOOP:   MOV A, M
         INR H
         CMP M
         JNZ LOOP1
-        MVI A, 01H
+        INR L
+        DCR H
+        DCR C
+        JNZ LOOP1
+        MOV A, D
         CALL MODIDT
-        JMP LOOP2
+        HLT
 LOOP1:  MVI A, 00H
         CALL MODIDT
-LOOP2:  DCR H 
-        INX H
-        DCR C 
-        JNZ LOOP 
-        RST 1
+        HLT
