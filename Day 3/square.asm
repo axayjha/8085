f36aa415b41a3e3d8ee6202daf165f2dd8148f2f
Akshay Jha <akshayjha@live.in>
2017-08-26 21:20:52 +0530

    Update square.asm

--- Day 3/square.asm
+++ Day 3/square.asm
@@ -1,13 +1,13 @@
 ; WAP to fine the square of a number using table look-up method.
 ; Table is stored from E100 onwards. Take user input from keypad
 ; and display result in data field; 
-; Assume table is like: E000 = 00, E001= 01, E002 = 04, E003 = 09, E004 = 16 .....
-
-MVI A, 0EH
-SIM
-CALL RDKBD
-MVI H, E1H
-MOV L, A 
-MOV A, M 
-CALL MODIDT 
-HLT
+; Assume table is like: E110 = 00, E111= 01, E112 = 04, E113 = 09, E114 = 16 .....
+      
+      MVI A, 0EH
+LOOP: CALL RDKBD
+      LXI H, E110H
+      ORA L
+      MOV L, A
+      MOV A, M
+      CALL MODIDT
+      JMP LOOP
