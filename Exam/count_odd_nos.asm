5d6696c839151ef125d8cfeecbe13832f25b98a3
Akshay Jha <akshayjha@live.in>
2017-10-29 23:27:18 +0530

    Update count_odd_nos.asm

--- Exam/count_odd_nos.asm
+++ Exam/count_odd_nos.asm
@@ -1,14 +1,14 @@
 ; Count the number of odd numbers in a 16 byte data block starting from E100 and store the result in E200
 		
-		LXI H, E100H	; Loading the starting address of data block
+		LXI H, E100H		; Loading the starting address of data block
 		MVI C, 10H		; loop counter = 16
 		MVI D, 00H		; result = 0
-LOOP:	MOV A, M        ; moving data from memory to accumulator
-		RRC				; shifting left, so that LSB goes to Cy
+LOOP:		MOV A, M        	; moving data from memory to accumulator
+		RRC			; shifting left, so that LSB goes to Cy
 		JNC SKIP 		; if Cy is 0, i.e, LSB was 0, means number is even. So, skip incrementing result
 
 		INR D 			; increment result value
-SKIP:	INX H   		; increment memory address
+SKIP:		INX H   		; increment memory address
 		DCR C 			; decrement counter
 		JNZ LOOP 		; if counter != 0, loop back
 		MOV A, D  		; move result to A
