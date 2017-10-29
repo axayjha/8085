a5b2730b87ed13b3264417f8a8f950735c9d8765
Akshay Jha <akshayjha@live.in>
2017-10-29 15:03:40 +0530

    Update multiply.asm

--- Day 3/multiply.asm
+++ Day 3/multiply.asm
@@ -1,13 +1,16 @@
-        LDA E100H
-        MOV B, A 
-        LDA E101H
-        MOV C, A 
-        CPI 00
+        LDA E100H       # Load first number
+        MOV B, A        # Move to B
+        CPI 00          # if number is 0, jump to end and result=0
+        JZ END  
+        LDA E101H       # Load second number
+        MOV C, A        # Move to C
+        CPI 00          # if number is 0, jump to end and result=0
         JZ END
-        XRA A 
-LOOP:   ADD B
-        DCR C
-        JZ  END
-        JMP LOOP
-END:    STA E102H
+        
+        XRA A           # A XOR A => A = 0
+LOOP:   ADD B           # A=A+B
+        DCR C           # C = C-1
+        JZ  END         # if C==0, jump to end and store result
+        JMP LOOP        # Loop back n add again
+END:    STA E102H       # store result in memory
         RST 1
