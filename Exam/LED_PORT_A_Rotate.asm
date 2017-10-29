2fb2624d5bef0ad8072cb84a09ecb779a3de57f7
Akshay Jha <akshayjha@live.in>
2017-10-29 23:20:37 +0530

    Updated comment syntax
    Replaced # with ;
    Makes it easier to work with simualtor on direct copy-paste

--- Exam/LED_PORT_A_Rotate.asm
+++ Exam/LED_PORT_A_Rotate.asm
@@ -1,23 +1,23 @@
-# lights port A LEDs, one by one from right to left and back to right end and so on 	   
+; lights port A LEDs, one by one from right to left and back to right end and so on 	   
 	   
 	   MVI A,80 
-	   OUT 33 		# OUT CSR
-	   MVI A,01		# to light up just one right-most LED (00000001)
+	   OUT 33 		; OUT CSR
+	   MVI A,01		; to light up just one right-most LED (00000001)
 
 MAIN:  
-# to go from right to left
-	   MVI C,07     # counter to iterate over n light up remaining 7 LEDs   
+; to go from right to left
+	   MVI C,07     ; counter to iterate over n light up remaining 7 LEDs   
 
-LOOP1: OUT 30       # send data from accumulator to port A
-	   RLC			# Shift accumulator data byte left
-	   MOV D,A      # Saving accumulator value as DELAY subroutine destroys data in A
-	   CALL DELAY   # to keep the LED lit for a moment
-	   MOV A,D      # Restoring accumulator value 
-	   DCR C        # decrement counter
-	   JNZ LOOP1    # break loop if counter == 0, else loop back
+LOOP1: OUT 30       ; send data from accumulator to port A
+	   RLC			; Shift accumulator data byte left
+	   MOV D,A      ; Saving accumulator value as DELAY subroutine destroys data in A
+	   CALL DELAY   ; to keep the LED lit for a moment
+	   MOV A,D      ; Restoring accumulator value 
+	   DCR C        ; decrement counter
+	   JNZ LOOP1    ; break loop if counter == 0, else loop back
 
-# to go back right from left end
-# similar logic as explained above
+; to go back right from left end
+; similar logic as explained above
 	   MVI B,07 
 
 LOOP2: OUT 30
@@ -27,12 +27,12 @@ LOOP2: OUT 30
 	   MOV A,D
 	   DCR B
 	   JNZ LOOP2
-# one round done	   
-# loop back to main to restart the whole process again	   
+; one round done	   
+; loop back to main to restart the whole process again	   
 	   JMP MAIN
 
 
-#--------------------------------------------
+;--------------------------------------------
 DELAY: LXI H,FFFF
 BACK:  DCX H
 	   MOV A,L
--- Exam/count_1s.asm
+++ Exam/count_1s.asm
@@ -1,14 +1,14 @@
-# Count the numbers of 1s in a byte of data
+; Count the numbers of 1s in a byte of data
 
-        LDA E100H		# Load number
-        MVI C, 08H		# loop counter = 8
-        MVI D, 00H		# Result = 0
+        LDA E100H		; Load number
+        MVI C, 08H		; loop counter = 8
+        MVI D, 00H		; Result = 0
 
-LOOP:   RAL				# Shift left through carry
-        JNC SKIP		# Jump on not carry (jump if bit is 0)
-        INR D 			# increment result  if bit == 1
-SKIP:   DCR C 			# decrement loop counter
-        JNZ LOOP		# if counter != 0 : loop back
-        MOV A, D  		# move result to A
-        STA E200H		# store result
+LOOP:   RAL				; Shift left through carry
+        JNC SKIP		; Jump on not carry (jump if bit is 0)
+        INR D 			; increment result  if bit == 1
+SKIP:   DCR C 			; decrement loop counter
+        JNZ LOOP		; if counter != 0 : loop back
+        MOV A, D  		; move result to A
+        STA E200H		; store result
         RST 1                
