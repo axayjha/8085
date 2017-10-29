15094199b8c1d1ae4e37cae65ab0e5574d6ee01f
Akshay Jha <akshayjha@live.in>
2017-10-29 20:09:11 +0530

    Add files via upload

---
+++ Exam/smallest_number.asm
@@ -0,0 +1,13 @@
+			LXI H, E100		; Starting address of the array
+			MVI C, 10		; Counter=16
+			MOV D, M		; The smallest number
+LOOP:		MOV A, M 		; Load the number in A
+			CMP D 			; Compare the current smallest with A
+			JNC SKIP 		; If A>D: skip setting A=D
+			MOV D, A 		; Set A=D, the current smallest
+SKIP:		INX H			; Increment address
+			DCR C 			; Decrement counter
+			JNZ LOOP	    ; If C!=0, loop back	
+			MOV A, D 		; Store result
+			STA E200
+			RST 1	
