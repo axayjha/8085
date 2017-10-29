321f37d2704cfc86204052bf8310be42b99c8272
Akshay Jha <akshayjha@live.in>
2017-10-29 23:24:23 +0530

    Update largest_number.asm

--- Exam/largest_number.asm
+++ Exam/largest_number.asm
@@ -1,11 +1,11 @@
 			LXI H, E100		; Starting address of the array
 			MVI C, 10		; Counter=16
 			MOV D, M		; The largest number
-LOOP:		MOV A, M 		; Load the number in A
+LOOP:			MOV A, M 		; Load the number in A
 			CMP D 			; Compare the current largest with A
 			JC SKIP 		; If A<D: skip setting A=D
 			MOV D, A 		; Set A=D, the current largest
-SKIP:		INX H			; Increment address
+SKIP:			INX H			; Increment address
 			DCR C 			; Decrement counter
-			JNZ LOOP	    ; If C!=0, loop back	
+			JNZ LOOP	    	; If C!=0, loop back	
 			RST 1	
