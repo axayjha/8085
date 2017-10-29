f4ba8d99a7a1b63f071d71cefb88353b2ce754a0
Akshay Jha <akshayjha@live.in>
2017-10-29 13:56:55 +0530

    Add files via upload

--- Day 1/subtraction.asm
+++ Day 1/subtraction.asm
@@ -1,13 +1,13 @@
-        MVI C, 00H
-        LDA E100H
-        MOV B, A 
-        LDA E101H
-        SUB B
-        JP LOOP
-        CMA
-        INR A
-        INR C 
-LOOP:   STA E102H 
-        MOV A, C 
-        STA E103H
-        RST 1 
+        MVI C, 00H   # borrow
+        LDA E100H    # get first number       
+        MOV B, A     # save it in B
+        LDA E101H    # get second number
+        SUB B        # A= A-B
+        JP LOOP      # if result is +ve, skip complementing
+        CMA          # complement
+        INR A        # add one to get 2's complement
+        INR C        # saving borrow
+LOOP:   STA E102H    # saving the result
+        MOV A, C     # moving the borrow to A
+        STA E103H    # saving the borrow
+        RST 1        # stop
