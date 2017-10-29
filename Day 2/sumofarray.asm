292c3062da54afed8d3d419c7673d16ddf54fffc
Akshay Jha <akshayjha@live.in>
2017-10-29 19:53:04 +0530

    Update sumofarray.asm

--- Day 2/sumofarray.asm
+++ Day 2/sumofarray.asm
@@ -1,28 +1,12 @@
-
-        MVI D, 10H                 
-        MVI E, 00H
-        MVI B, E1H
-
-LOOP:   
-        MOV H, B
-        MOV L, E
-        MOV C, M
-        
-        MOV A, B
-        ADI 1H
-        MOV H, A
-        MOV A, M
-
-        ADD C
-        MOV C, A
-
-        MOV A, B
-        ADI 2H
-        MOV H, A
-        MOV M, C
-
-        INR E  
-        DCR D
-        JNZ LOOP
-        RST 1
-
+        LXI H, E100     # Load the starting address of first array/block
+        MVI C, 10       # counter C=16
+LOOP:   MOV A, M        # load number from first block in A
+        INR H           # increment address from E1XX to E2XX to load second number in M
+        ADD M           # add second number to first (A=A+M)
+        INR H           # increment address to E3XX
+        MOV M, A        # save the result in third block
+        DCR H           # decrement address
+        DCR H           # back to E1XX
+        INR L           # increment lower byte (XX part)
+        DCR C           # decrement counter
+        JNZ LOOP        $ if C!=0, loop back
