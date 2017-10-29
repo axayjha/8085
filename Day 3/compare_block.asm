f2443d9be42aa9c10037ca77540cf86bf3c9d9c1
Akshay Jha <akshayjha@live.in>
2017-10-29 14:56:44 +0530

    Update compare_block.asm

--- Day 3/compare_block.asm
+++ Day 3/compare_block.asm
@@ -6,7 +6,7 @@
 CHECKAGAIN:     MOV A, M              # moving data from first array to A
                 INR H                 # incrementing E1 to E2
                 CMP M                 # comparing data in second array with data from first array (which is in A right now)
-                JNZ SKIP              # if doesnt match, print 0 (False) straight, away
+                JZ SKIP               # if doesnt match, print 0 (False) straight, away
                 INR L                 # else increment the last byte of the address
                 DCR H                 # decrementing E2 to E1
                 DCR C                 # decrementing counter
