75a550be9733247e90d7fff4a544902ead6b872e
Akshay Jha <akshayjha@live.in>
2017-11-26 17:49:48 +0530

    Update multiplication_w_carry.asm

--- Exam/multiplication_w_carry.asm
+++ Exam/multiplication_w_carry.asm
@@ -5,11 +5,12 @@
         LDA E101H       ; Load second number
         CPI 00          ; if number is 0, jump to end and result=0
         JZ END
-        MOV C, A        ; Move second number to C
+        
         
         XRA A           ; A XOR A => A=0
 
-LOOP:   ADD B           ; A = A + B
+LOOP:   MOV C, A        ; Move second number to C
+        ADD B           ; A = A + B
         DCR C           ; C = C-1
         JZ  END         ; if C==0, jump to end and store result
         JMP LOOP        ; else, loop back and add
