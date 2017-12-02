c0198d2b576d5db9849af9f2bfbd237938ad69ed
Akshay Jha <akshayjha@live.in>
2017-12-02 16:38:20 +0530

    Update and rename Fibonacci.asm to fibonacci.asm

---
+++ Extras/fibonacci.asm
@@ -0,0 +1,17 @@
+; credits: Rishabh Shairy
+; save 0 and 1 first in E100H and E101H respectively
+
+START:	   MVI C,09	; counter (to get 9 fib terms)
+	   LXI H,E100	
+
+LOOP:	 MOV A,M
+	   INX H
+	   MOV B,M
+	   INX H
+	   ADD B
+	   DAA
+	   MOV M,A
+	   DCX H
+	   DCR C
+	   JNZ LOOP
+	   RST 1
