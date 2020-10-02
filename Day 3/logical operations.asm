8f2404049b4e083b7258ead51657996553226385
Swastik Khuntia <swostikkhuntia@gmail.com>
2020-10-02 17:42:43 +0530

    Update logical operations.asm
    Added what each instruction does as comments in the program itself.

--- Day 3/logical operations.asm
+++ Day 3/logical operations.asm
@@ -1,13 +1,13 @@
-MVI A,9EH
-ANI 05H
-STA 2500H
-MVI A,9EH
-ORI 07H
-STA 2501H
-MVI A,9EH
-XRI 09H
-STA 2502H
-MVI A,9EH
-CMA
-STA 2503H
-HLT
+MVI A,9EH /*Moves data 9E in hexadecimal (9EH) to the accumulator*/
+ANI 05H /*Perform logical operation AND with immediate data and the data loaded in accumulator*/
+STA 2500H /*Stores the result of the logical operation(stored in accumulator)to the memory location whose address is 2500H*/
+MVI A,9EH /*Moves data 9E in hexadecimal (9EH) to the accumulator*/
+ORI 07H /*Perform logical operation OR with immediate data and the data loaded in accumulator*/
+STA 2501H /*Stores the result of the logical operation(stored in accumulator)to the memory location whose address is 25001H*/
+MVI A,9EH /*Moves data 9E in hexadecimal (9EH) to the accumulator*/
+XRI 09H /*Perform logical operation EX-OR with immediate data and the data loaded in accumulator*/
+STA 2502H /*Stores the result of the logical operation(stored in accumulator)to the memory location whose address is 2502H*/
+MVI A,9EH /*Moves data 9E in hexadecimal (9EH) to the accumulator*/
+CMA /*Perform logical operation NOT(Complement) with immediate data and the data loaded in accumulator*/
+STA 2503H /*Stores the result of the logical operation(stored in accumulator)to the memory location whose address is 2503H*/
+HLT /*End*/
