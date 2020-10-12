561615a3373c1ebd3db0f9d15b1d1e8832aa037e
Akshay Anand <akshayjha@live.in>
2020-10-12 21:37:17 +0530

    adding comments

--- Day 3/largest_in_series.asm
+++ Day 3/largest_in_series.asm
@@ -1,13 +1,15 @@
-LXI H,3000H
-MOV C,M
-INX H
-MOV A,M
-DCR C
-LOOP: INX H
-CMP M
-JNC SKIP
-MOV A,M
-SKIP: DCR C
-JNZ LOOP
-STA 300AH
-HLT
+; Program to find the largest number in an array
+
+		    LXI H, 3000H  ; Load address in HL
+    		MOV C, M      ; Store counter in regC
+		    INX H 		    ; Increment pointer
+		    MOV A, M      ; Load first number in Acc
+		    DCR C         ; Decrement counter
+LOOP: 	INX H  		    ; Increment pointer
+		    CMP M         ; Compare number with Acc
+		    JNC SKIP	    ; If Acc is greater, move on
+		    MOV A, M      ; Else number pointed by current ptr is larger
+SKIP:	  DCR C         ; Decrement counter
+		    JNZ LOOP      ; loop back if counter not zero
+		    STA 3A00H     ; Store result
+		    HLT 
